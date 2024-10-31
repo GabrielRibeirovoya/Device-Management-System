25f677abba5bf19c9144fe8c13fbaa3f134418b7
Gabriel Ribeiro <gabrielsribeiro3312@gmail.com>
2024-10-30 22:29:00 -0300

    Primeiro Commit

--- Monitor/Form1.Designer.cs
+++ Monitor/Form1.Designer.cs
@@ -29,19 +29,50 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            statusStrip1 = new StatusStrip();
+            hostname = new ToolStripStatusLabel();
+            Operating_system = new ToolStripStatusLabel();
+            statusStrip1.SuspendLayout();
             SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { hostname, Operating_system });
+            statusStrip1.Location = new Point(0, 428);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(800, 22);
+            statusStrip1.TabIndex = 0;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // hostname
+            // 
+            hostname.Name = "hostname";
+            hostname.Size = new Size(0, 17);
+            // 
+            // Operating_system
+            // 
+            Operating_system.Name = "Operating_system";
+            Operating_system.Size = new Size(0, 17);
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(statusStrip1);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
             Text = "Device Management System";
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel hostname;
+        private ToolStripStatusLabel Operating_system;
     }
 }
